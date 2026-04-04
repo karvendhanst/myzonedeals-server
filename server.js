aaf62d9af5090fe67475ae59eb696dfddd1437b7
karvendhan-Iaaxin <karvendhan@iaaxin.com>
2026-04-04 15:51:13 +0530

    fix cors

--- server.js
+++ server.js
@@ -14,8 +14,24 @@ app.use(express.json());
 
 connectDB();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://myzonedeals-client.vercel.app",
+];
 
-app.use(cors(),);
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+}));
 
 app.get("/api/health", (_, res) => {
   res.json({ msg: "app is running well !!!" });
