dac33f02b361a65cc9662122e8d23b9caed7dbc5
karvendhan-Iaaxin <karvendhan@iaaxin.com>
2026-05-13 13:24:23 +0530

    fix smtp

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,12 +2,16 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
