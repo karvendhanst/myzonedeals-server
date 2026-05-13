87f7a3580a2b1b6c468c4dc81a4b9db67c9274b4
karvendhan-Iaaxin <karvendhan@iaaxin.com>
2026-05-13 17:12:40 +0530

    fix mail family

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,3 +1,36 @@
+// import nodemailer from "nodemailer";
+// import dns from "dns";
+
+// dns.setDefaultResultOrder("ipv4first");
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log("SMTP ERROR:", error);
+//   } else {
+//     console.log("SMTP SERVER READY");
+//   }
+// });
+
+// export const sendEmail = async (to, subject, html) => {
+//   await transporter.sendMail({
+//     from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//   });
+// };
+
+
 import nodemailer from "nodemailer";
 import dns from "dns";
 
@@ -7,10 +40,17 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  family: 4, 
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
@@ -22,11 +62,16 @@ transporter.verify((error, success) => {
 });
 
 export const sendEmail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
-};
+  try {
+    const info = await transporter.sendMail({
+      from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
 
+    console.log("EMAIL SENT:", info.messageId);
+  } catch (error) {
+    console.log("SEND EMAIL ERROR:", error);
+  }
+};
