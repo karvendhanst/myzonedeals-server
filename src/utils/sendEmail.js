993982162ebbf0ac350ccd28a6453d9664fe7838
karvendhan-Iaaxin <karvendhan@iaaxin.com>
2026-05-13 14:41:21 +0530

    fix mail issue

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer";
 import dns from "dns";
 
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
