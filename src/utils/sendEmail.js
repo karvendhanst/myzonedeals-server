9f536543f31a7361acd100b2a93c63f0791588b1
karvendhan-Iaaxin <karvendhan@iaaxin.com>
2026-05-13 17:24:39 +0530

    implememt brevo

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,3 +1,36 @@
+// // import nodemailer from "nodemailer";
+// // import dns from "dns";
+
+// // dns.setDefaultResultOrder("ipv4first");
+
+// // const transporter = nodemailer.createTransport({
+// //   host: "smtp.gmail.com",
+// //   port: 587,
+// //   secure: false,
+// //   auth: {
+// //     user: process.env.EMAIL_USER,
+// //     pass: process.env.EMAIL_PASS,
+// //   },
+// // });
+
+// // transporter.verify((error, success) => {
+// //   if (error) {
+// //     console.log("SMTP ERROR:", error);
+// //   } else {
+// //     console.log("SMTP SERVER READY");
+// //   }
+// // });
+
+// // export const sendEmail = async (to, subject, html) => {
+// //   await transporter.sendMail({
+// //     from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
+// //     to,
+// //     subject,
+// //     html,
+// //   });
+// // };
+
+
 // import nodemailer from "nodemailer";
 // import dns from "dns";
 
@@ -7,10 +40,15 @@
 //   host: "smtp.gmail.com",
 //   port: 587,
 //   secure: false,
+
 //   auth: {
 //     user: process.env.EMAIL_USER,
 //     pass: process.env.EMAIL_PASS,
 //   },
+
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000,
 // });
 
 // transporter.verify((error, success) => {
@@ -22,35 +60,32 @@
 // });
 
 // export const sendEmail = async (to, subject, html) => {
-//   await transporter.sendMail({
-//     from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
-//     to,
-//     subject,
-//     html,
-//   });
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+
+//     console.log("EMAIL SENT:", info.messageId);
+//   } catch (error) {
+//     console.log("SEND EMAIL ERROR:", error);
+//   }
 // };
 
 
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
 
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_EMAIL,
+    pass: process.env.BREVO_PASS,
   },
-
-  family: 4, 
-
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
@@ -64,7 +99,7 @@ transporter.verify((error, success) => {
 export const sendEmail = async (to, subject, html) => {
   try {
     const info = await transporter.sendMail({
-      from: `"My Zone Deals" <${process.env.EMAIL_USER}>`,
+      from: `"My Zone Deals" <${process.env.BREVO_EMAIL}>`,
       to,
       subject,
       html,
