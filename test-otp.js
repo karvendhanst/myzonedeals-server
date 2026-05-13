38e464656e744d96522237ceced8dd36da65d2b3
KARVENDHAN <imkarvendhan@gmail.com>
2026-05-13 12:12:56 +0530

    Delete test-otp.js

--- test-otp.js
+++
@@ -1,9 +0,0 @@
-import otpGenerator from "otp-generator";
-
-console.log("Generating OTP...");
-const otp = otpGenerator.generate(6, {
-  upperCaseAlphabets: false,
-  lowerCaseAlphabets: false,
-  specialChars: false,
-});
-console.log("OTP:", otp);
