ab4bfd981c5a0e6f3091ad9a92935eb0a98e654d
karvendhan-Iaaxin <karvendhan@iaaxin.com>
2026-05-17 20:07:12 +0530

    fix mail 4

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -76,14 +76,12 @@
 
 
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
 
   auth: {
     user: process.env.BREVO_EMAIL,
